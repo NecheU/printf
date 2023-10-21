28c15119ab488ba48f910d070c3bfbd424452e07
Uzor <uzorgodsboy1@gmail.com>
2023-10-20 23:01:23 -0700

    printf_rot_13.c

--- printf_rot_13.c
+++ printf_rot_13.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * printf_rot_13 - it converra a char to rot13
- * @char_rot: argument
- *
- * Return: counter
- */
+* printf_rot_13 - it converra a char to rot13
+* @char_rot: argument
+*
+* Return: counter
+*/
 
 int printf_rot_13(va_list char_rot)
 {
-	int y, z, counter = 0;
-	int h = 0;
+int y, z, counter = 0;
+int h = 0;
 
-	char *s = va_arg(char_rot, char*);
-	char al[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char bl[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
-	
-	if (s == NULL)
-	for (y = 0; s[y]; y++)
-	{
-		h = 0;
-		for (z = 0; al[z] && !h; z++)
-		{
-			if (s[y] == al[z])
-			{
-				_putchar(bl[z]);
-				counter++;
-				h = 1;
-			}
-		}
-		if (!h)
-		{
-			_putchar(s[y]);
-			counter++;
-		}
-	}
-	return (counter);
+char *s = va_arg(char_rot, char*);
+char al[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+char bl[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+if (s == NULL)
+for (y = 0; s[y]; y++)
+{
+h = 0;
+for (z = 0; al[z] && !h; z++)
+{
+if (s[y] == al[z])
+{
+_putchar(bl[z]);
+counter++;
+h = 1;
+}
+}
+if (!h)
+{
+_putchar(s[y]);
+counter++;
+}
+}
+return (counter);
 }
