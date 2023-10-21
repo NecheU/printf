4bcce5b4d8aa0ba365a949a2aca5e82f3a3f102a
Uzor <uzorgodsboy1@gmail.com>
2023-10-20 21:30:03 -0700

    main.h

--- main.h
+++ main.h
@@ -14,11 +14,9 @@ int _strlenc(const char *str);
 int printf_37(void);
 int printf_rot_13(va_list char_rot);
 int printf_str_reverse(va_list argm);
-<<<<<<< HEAD
 int get_flag(char s, flags_t *f);
-
-=======
-nt print_address(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *f);
 char *convert(unsigned long int num, int base, int lowercase);
->>>>>>> 11a762c1f7a020dd6d3c5b8858f6acd5ebc8c49b
+
+
 #endif
